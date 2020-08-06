9bd2267015686b8cd3890f5b6713ae90a6e3c2ea
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-08-06 03:45:06 +0100

    Fix SetGooleTest

--- SetGoogleTest.sh
+++ SetGoogleTest.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/google/googletest.git
-cd googletest
-make
-sudo cp *.a /usr/lib
+sudo apt install libgtest-devcd googletest
+cd /usr/src/gtest sudo && cmake CMakeLists.txt && sudo make
+sudo cp libgtest.a libgtest_main.a /usr/lib
