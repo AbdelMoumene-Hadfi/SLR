c116eddadbf9a143a09be031db00e2ae05d8e77b
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-08-06 03:51:45 +0100

    Fix SetGoogleTest

--- SetGoogleTest.sh
+++ SetGoogleTest.sh
@@ -1,6 +1,8 @@
 sudo apt-get install cmake
 git clone https://github.com/google/googletest.git
 cd googletest-master
-sudo cmake CMakeLists.txt
+ls
+sudo -E cmake CMakeLists.txt
+ls
 make
 sudo cp *.a /usr/lib
