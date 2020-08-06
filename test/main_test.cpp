7d0699457d3f58e625e0504821e2a17393caa093
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-08-06 05:06:43 +0100

    Add some test on main_test

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -6,9 +6,13 @@ TEST(ReadFile,Axiom)  {
   EXPECT_EQ('E',Mygrammer.axiom);
 };
 
+TEST(ReadFile,Terminal)  {
+  Mygrammer.readTerminal()
+  EXPECT_EQ("+ * ( ) e \n",Mygrammer.showTerminal());
+};
 
 int main(int argc,char**argv) {
-    Mygrammer.file.open("../grammar");
+    Mygrammer.file.open("grammar");
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
