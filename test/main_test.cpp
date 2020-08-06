ca6224ccffa47888792050c3d5de167cd9e4dd8b
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-08-06 19:04:59 +0100

    Add main_test

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -5,9 +5,23 @@ TEST(ReadFile,Axiom)  {
   Mygrammer.getAxiom();
   EXPECT_EQ('E',Mygrammer.axiom);
 };
-
 TEST(ReadFile,Terminal)  {
   EXPECT_EQ(0,Mygrammer.readTerminal());
+};
+TEST(ReadFile,noTerminal)  {
+  EXPECT_EQ(0,Mygrammer.readNoTerminal());
+};
+TEST(ReadFile,Rules)  {
+  EXPECT_EQ(0,Mygrammer.readRules());
+};
+TEST(ReadFile,Rules)  {
+  EXPECT_EQ(0,Mygrammer.readRules());
+};
+TEST(GrammarFunction,isTerminal)  {
+  EXPECT_EQ(true,Mygrammer.isTerminal('+'));
+};
+TEST(GrammarFunction,ruleNumber)  {
+  EXPECT_EQ(1,Mygrammer.ruleNumber(std::string("E->E+T")));
 };
 
 int main(int argc,char**argv) {
